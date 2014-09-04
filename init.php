9049660d2a8555f181e1c6e3298320823d8b7080
setimouse <setimouse@gmail.com>
2014-09-04 18:35:09 +0800

    smarty baseaction

--- init.php
+++ init.php
@@ -3,3 +3,4 @@ define('BEGLOBAL', dirname(__FILE__).'/');
 define('GLCONFIG', BEGLOBAL.'config/');
 define('GLMODEL', BEGLOBAL.'model/');
 define('GLLIB', BEGLOBAL.'lib/');
+define('GUTIL', BEGLOBAL.'util/');
---
+++ model/SmartyBaseAction.class.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * description
+ *
+ * Filename: SmartyBaseAction.class.php
+ *
+ * @author liyan
+ * @since 2014 9 3
+ */
+
+require_once GUTIL.'smarty/Smarty.class.php';
+require_once GUTIL.'smarty/SmartyBC.class.php';
+
+abstract class SmartyBaseAction extends XBaseAction {
+
+    protected static $smarty;
+
+    function __construct() {
+        parent::__construct();
+        $this->smarty = new Smarty();
+    }
+
+    protected static function getView() {
+        if (!self::$smarty) {
+            self::$smarty = new Smarty();
+        }
+        return self::$smarty;
+    }
+
+    protected function assign($key, $value) {
+        self::getView()->assign($key, $value);
+    }
+
+    /**
+     * 渲染模板
+     *
+     * @param string $template
+     */
+    protected function display($template) {
+        foreach ($this->arrHeader as $key => $value) {
+            header($key.":".$value);
+        }
+
+        self::getView()->display($template);
+    }
+}
+
