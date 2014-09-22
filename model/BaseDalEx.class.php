04714912d66c2f0a01891bf80235e77f2b67cf2d
setimouse <setimouse@gmail.com>
2014-09-22 12:25:04 +0800

    dal try

---
+++ model/BaseDalEx.class.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * description
+ *
+ * Filename: BaseDalEx.class.php
+ *
+ * @author liyan
+ * @since 2014 9 16
+ */
+abstract class BaseDalEx extends BaseDal {
+
+    protected static function result($ret, $dbProxy) {
+        if (false === $ret) {
+            throw new DBException($dbProxy->error(), $dbProxy->errno());
+        }
+        return $ret;
+    }
+
+}
---
+++ model/DBException.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * description
+ *
+ * Filename: DBException.class.php
+ *
+ * @author liyan
+ * @since 2014 9 16
+ */
+class DBException extends Exception {
+
+
+
+}
