7244face18780a221b6cacc4483a5b503f77b0a7
setimouse <setimouse@gmail.com>
2014-10-16 10:27:14 +0800

    iobserverdelegate

---
+++ model/IObserverDelegate.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * observer delegate
+ *
+ * Filename: IObserverDelegate.class.php
+ *
+ * @author liyan
+ * @since 2014 10 15
+ */
+interface IObserverDelegate {
+
+    public function observerPath();
+
+}
