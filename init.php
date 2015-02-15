ba81f43cc81a55e41d392ef3c0c2581478bf4241
Li Yan <liyan34@lenovo.com>
2015-02-15 16:03:30 +0800

    rebuild

--- init.php
+++ init.php
@@ -3,4 +3,14 @@ define('BEGLOBAL', dirname(__FILE__).'/');
 define('GLCONFIG', BEGLOBAL.'config/');
 define('GLMODEL', BEGLOBAL.'model/');
 define('GLLIB', BEGLOBAL.'lib/');
-define('GUTIL', BEGLOBAL.'util/');
+define('GLUTIL', BEGLOBAL.'util/');
+
+DAutoloader::addAutoloadPathArray(
+    array(
+        GLLIB,
+        GLMODEL,
+        GLUTIL,
+    )
+);
+
+Config::loadConfig(GLCONFIG.'global');
